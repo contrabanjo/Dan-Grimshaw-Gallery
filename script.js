7b7c2b7649bce203aa3d35cd2af8184f03dde351
Corrie Grimshaw <corrie.e.grimshaw@gmail.com>
2021-11-15 17:36:19 -0800

    Feature: Added modal to allow fullscren viewing of pictures. Also cleaned up styling/display of photos.

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let parent = document.getElementById("image-preview");
 for(i = 1; i <= numberOfPicturesInFolder; i++){
    let newImg = document.createElement('img');
    newImg.src = "./images-50/" + i + ".jpg";
+   newImg.name = i + ".jpg";
    parent.appendChild(newImg);
 }
 
@@ -40,8 +41,8 @@ document.querySelectorAll("#image-preview img").forEach(
 
 
 function sidebarChildClick(e){
-    document.getElementById("gallery").classList.toggle("hidden");
-    document.getElementById("contact-page").classList.toggle("hidden");
+    document.getElementById("gallery-container").hidden = false;
+    document.getElementById("contact-page-container").hidden = true;
 
     e.target.classList.add("selected");
     document.querySelectorAll("#sidebar :not(:first-child)").forEach((t)=>{
@@ -51,12 +52,32 @@ function sidebarChildClick(e){
     })
 
     if (e.target.id==="contact-button") {
-        console.log("contact button triggered");
-         document.getElementById("gallery").classList.add = "hidden";
-         document.getElementById("contact-page").classList.remove = "hidden";
+        document.getElementById("gallery-container").hidden = true;
+        document.getElementById("contact-page-container").hidden = false;
     }
 }
 
 document.querySelectorAll("#sidebar :not(:first-child)").forEach((e)=>{
     e.addEventListener("click", sidebarChildClick)
+})
+
+function showFullsize(e){
+    const modal = document.createElement('div');
+    modal.id = "fullsize-modal";
+
+    const img = document.createElement('img');
+    img.id = "fullsize-img";
+    img.src = "./images/" + e.target.name;
+
+    const closeButton = document.createElement('p');
+    closeButton.innerHTML = "X"
+    closeButton.addEventListener("click", ()=>{document.getElementById("fullsize-modal").remove()})
+
+    modal.appendChild(closeButton);
+    modal.appendChild(img);
+    document.getElementById("container").appendChild(modal);
+}
+
+document.querySelectorAll("#image-preview img").forEach(e => {
+    e.addEventListener("click", showFullsize);
 })
